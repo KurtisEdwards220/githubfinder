3806033012481b0bc18377abebe0ae30665c28e2
Kurtis Edwards <edwards.kurtis@gmail.com>
2023-03-02 04:17:27 -0500

    fetch and display repos

--- github.js
+++ github.js
@@ -2,16 +2,25 @@ class Github {
   constructor() {
     this.client_id = '2dabcabbedf9ba8d807b';
     this.client_secret = '930b0a550320cbf2dc999ce28ad2a0ac071c4b39';
+    this.repos_count = 5;
+    this.repos_sort = 'created: asc';
   }
 
   async getUser(user) {
     const profileResponse = await fetch(
       `https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`
     );
+
+    const repoResponse = await fetch(
+      `https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`
+    );
+
     const profile = await profileResponse.json();
+    const repos = await repoResponse.json();
 
     return {
       profile,
+      repos,
     };
   }
 }
