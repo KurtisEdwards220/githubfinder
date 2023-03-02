7a05d7fb587f19d02364189263d47fb92b165e03
Kurtis Edwards <edwards.kurtis@gmail.com>
2023-03-02 00:05:22 -0500

    add api

---
+++ github.js
@@ -0,0 +1,17 @@
+class Github {
+  constructor() {
+    this.client_id = '2dabcabbedf9ba8d807b';
+    this.client_secret = '0b2b4fdaaa45395436afbc416ad00a5e9bf95238';
+  }
+
+  async getUser(user) {
+    const profileResponse = await fetch(
+      `https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`
+    );
+    const profileData = await profileResponse.json();
+
+    return {
+      profile,
+    };
+  }
+}
